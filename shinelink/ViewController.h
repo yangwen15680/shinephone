a62202060c19e5099460ccf981a81ff7bf990246
sky <328657662@qq.com>
2016-02-15 14:23:47 +0800

    Initial Commit

---
+++ shinelink/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  shinelink
+//
+//  Created by sky on 16/2/15.
+//  Copyright © 2016年 sky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ shinelink/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  shinelink
+//
+//  Created by sky on 16/2/15.
+//  Copyright © 2016年 sky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
